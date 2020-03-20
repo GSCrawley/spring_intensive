8d5b0a183163f6d7a5ee1bed261f1b4b18f38feb
GSCrawley <gscrawley@gmail.com>
2020-03-19 22:24:21 -0700

    registered with Scaledrone and connected API

--- src/App.js
+++ src/App.js
@@ -21,21 +21,33 @@ function randomColor() {
 
 class App extends Component {
   state = {
-    messages: [ {
-      text: "This is a test message!",
-      member: {
-        color: "blue",
-        username: "bluemoon"
-      }
-    }
-  ],
+    messages: [],
     member: {
       username: randomName(),
       color: randomColor(),
     }
   }
 
-// constructor goes here
+  constructor() {
+    super();
+    this.drone = new window.Scaledrone("8pcGksl41f4QVxhq", {
+      data: this.state.member
+    });
+    this.drone.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      const member = {...this.state.member};
+      member.id = this.drone.clientId;
+      this.setState({member});
+    });
+    const room = this.drone.subscribe("observable-room");
+    room.on('data', (data, member) => {
+      const messages = this.state.messages;
+      messages.push({member, text: data});
+      this.setState({messages});
+    });
+  }
 
   render() {
     return (
@@ -55,12 +67,11 @@ class App extends Component {
   }
 
   onSendMessage = (message) => {
-    const messages = this.state.messages
-    messages.push({
-      text: message,
-      member: this.state.member
+    this.drone.publish({
+      room: "observable-room",
+      message
     });
-    this.setState({messages: messages})
   }
 }
-  export default App;
+
+export default App;
