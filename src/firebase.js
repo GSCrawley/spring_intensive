9008bece9f31af75732e2c26318dd0636fdee774
GSCrawley <gscrawley@gmail.com>
2020-07-26 22:48:42 -0700

    created UserProvider

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBt68W2a68uzvDkoHbLRYIqwrOVAtLW3k8",
+    authDomain: "gids-chat-app-auth.firebaseapp.com",
+    databaseURL: "https://gids-chat-app-auth.firebaseio.com",
+    projectId: "gids-chat-app-auth",
+    storageBucket: "gids-chat-app-auth.appspot.com",
+    messagingSenderId: "9965876555",
+    appId: "1:9965876555:web:bae664bdc9d64358b1e4ce",
+  };
